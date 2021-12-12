1e840babec7bff9587e6278a839e262740b1d013
asimkilic <a.asim.kilic@gmail.com>
2021-12-12 16:49:30 +0300

    Spring boot added and DB connection configurated

---
+++ secondWeek/spring-boot-training/src/main/java/com/asimkilic/springboot/springboottraining/SpringBootTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.asimkilic.springboot.springboottraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTrainingApplication.class, args);
+	}
+
+}
